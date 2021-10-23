afeafdad374a6ccbb69dc9548b3b148187ce98fc
Yajima Yohei <yajima.y.yohei@gmail.com>
2021-10-23 17:17:05 +0900

    Initial Commit

---
+++ Custom modal with scrollview/Custom_modal_with_scrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_modal_with_scrollviewApp.swift
+//  Custom modal with scrollview
+//
+//  Created by Yajima Youhei on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_modal_with_scrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
